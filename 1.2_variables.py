8265bd0ca7dab129d943a4d28e97bfed679598a8
Achmad Qizwini <akise.shun@gmail.com>
2023-04-18 20:36:56 +0700

    variable

---
+++ 1.2_variables.py
@@ -0,0 +1,15 @@
+data = 9.5
+a, b = "ular", "python"
+print(a, b, a + b)
+
+data_int = int(data)
+data_string = str(data)
+data_bool = bool(data)
+
+print(data, type(data))
+print(data_bool, type(data_bool))
+print(data_string, type(data_string))
+print(data_int, type(data_int))  # dibulatkan kebawah
+
+# bool false = 0
+# true = 1
