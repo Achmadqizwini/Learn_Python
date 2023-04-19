7eb64b0be01ae190249a656c1eb2db1fcf6e8e0f
Achmad Qizwini <akise.shun@gmail.com>
2023-04-19 12:08:36 +0700

    magic method

--- oop/magic_method.py
+++ oop/magic_method.py
@@ -9,9 +9,19 @@ class Mangga:
     def __str__(self) -> str:
         return "{} dengan jumlah {}".format(self.nama, self.jumlah)
 
+    def __add__(self, objek):
+        return self.jumlah + objek.jumlah
+
+    @property
+    def __dict__(self):
+        return "objek ini mempunyai jumlah"
+
 
 mangga1 = Mangga("arumanis", 1)
 mangga2 = Mangga("Manalagi", 2)
 
 print(repr(mangga1))
 print(mangga2)
+
+print(mangga1 + mangga2)
+print(mangga1.__dict__)
