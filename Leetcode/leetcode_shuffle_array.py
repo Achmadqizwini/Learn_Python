7b32b6158fecd4b20d4e7c1ebb33d5b614897cb9
Achmad Qizwini <akise.shun@gmail.com>
2023-04-20 16:56:42 +0700

    shuffle an array

---
+++ Leetcode/leetcode_shuffle_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def shuffle(nums: List[int], n: int) -> List[int]:
+    res = []
+    for i, j in zip(nums[:n], nums[n:]):
+        res += [i, j]
+    return res
+
+
+# Example usage
+nums = [1, 2, 3, 4, 5, 6]
+n = 3
+shuffled_nums = shuffle(nums, n)
+print(shuffled_nums)
+
+
+class Solution:
+    @staticmethod
+    def shuffle2(nums: List[int], n: int) -> List[int]:
+        print(list(zip(nums[:n], nums[n:])))
+        return [num for t in zip(nums[:n], nums[n:]) for num in t]
+
+
+# Example usage
+nums = [1, 2, 3, 4, 5, 6]
+n = 3
+shuffled_nums = Solution.shuffle2(nums, n)
+print(shuffled_nums)
