27db6a54677b757049cf6bfc92591029226105e3
Achmad Qizwini <akise.shun@gmail.com>
2023-04-29 23:38:54 +0700

    modify : user option 1 to read data

--- Simple App/main.py
+++ Simple App/main.py
@@ -1,7 +1,20 @@
 import os
 
+import crud
+
 if __name__ == "__main__":
     sistem_operasi = os.name
+
+    match sistem_operasi:
+        case "posix": os.system("clear")
+        case "nt": os.system("cls")
+
+    print("WELCOME TO THIS PROGRAM")
+    print("LIBRARY DATABASE")
+    print("=======================")
+
+    # check database ada atau tidak
+    crud.init_console()
 
     while (True):
 
@@ -23,7 +36,7 @@ if __name__ == "__main__":
         print("\n=======================\n")
 
         match user_option:
-            case "1": print("1. Read data")
+            case "1": crud.read_console()
             case "2": print("2. Create data")
             case "3": print("3. Update data")
             case "4": print("4. Delete data")
