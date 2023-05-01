4a0013ba7599d62f32b7e973381e3d69d5689789
Achmad Qizwini <akise.shun@gmail.com>
2023-05-01 17:24:35 +0700

    modify: add a route to delete

--- Simple project/django playground/django_project/blog/urls.py
+++ Simple project/django playground/django_project/blog/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='posts'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('post/create', views.create_post, name='post-create'),
+    path('post/edit/<int:id>/', views.edit_post, name='post-edit'),
+    path('post/delete/<int:id>/', views.delete_post, name='post-delete'),
 ]
