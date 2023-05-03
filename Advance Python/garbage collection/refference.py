a4d1ff91ee42336b5e7907c3ec1e37a038b09f3f
Achmad Qizwini <akise.shun@gmail.com>
2023-05-03 11:40:25 +0700

    threading

---
+++ Advance Python/garbage collection/refference.py
@@ -0,0 +1,22 @@
+import ctypes
+
+a = 100
+print(a)
+print(id(a))
+print(f'\nmemory address where value of a is assigned {hex(id(a))}')
+
+b = a
+print(f'memory address where value of b is assigned {hex(id(b))}')
+
+a = 0
+
+
+def ref_count(address):
+    return ctypes.c_long.from_address(address).value
+
+
+num = [1, 2, 3]
+numl = id(num)
+print(ref_count(numl))
+numk = num
+print(ref_count(numl))
