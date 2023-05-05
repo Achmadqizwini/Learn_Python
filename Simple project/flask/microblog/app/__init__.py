b7e7ed4008704e66c48c7ed954190c6ce239c39a
Achmad Qizwini <akise.shun@gmail.com>
2023-05-05 22:35:02 +0700

    modify: run database

--- Simple project/flask/microblog/app/__init__.py
+++ Simple project/flask/microblog/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-from config import Config
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 apps = Flask(__name__)
 apps.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(apps)
+migrate = Migrate(apps, db)
+
+from app import routes, models
