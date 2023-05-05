4100c02a31f75db8e69c17d1d3930aae1ee0b097
Achmad Qizwini <akise.shun@gmail.com>
2023-05-05 15:25:03 +0700

    add: login routes

--- Simple project/flask/microblog/app/routes.py
+++ Simple project/flask/microblog/app/routes.py
@@ -1,9 +1,12 @@
 from app import apps
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @apps.route('/')
 @apps.route('/index')
+
 def index():
+
     user = {'username': {"Achamd"}}
 
     posts = [
@@ -17,3 +20,12 @@ def index():
         },
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@apps.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('login requested for user{}, remember_me= {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
